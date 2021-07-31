7e7c8454c279a282167ab3364bf9c12d30f69e33
Kevs <kev.sunlee@gmail.com>
2021-07-31 16:51:55 -0700

    add LoginForm

---
+++ src/components/LoginForm.vue
@@ -0,0 +1,30 @@
+<template>
+  <form @submit.prevent="handleSubmit">
+    <input type="email" required placeholder="email" v-model="email" />
+    <input type="password" required placeholder="password" v-model="password" />
+    <button type="submit">Login</button>
+  </form>
+</template>
+
+<script>
+import { ref } from 'vue'
+
+export default {
+  setup() {
+    const email = ref('')
+    const password = ref('')
+
+    const handleSubmit = () => {
+      console.log('form', email.value, password.value)
+    }
+
+    return {
+      handleSubmit,
+      email,
+      password,
+    }
+  },
+}
+</script>
+
+<style></style>
--- src/views/Welcome.vue
+++ src/views/Welcome.vue
@@ -1,16 +1,66 @@
 <template>
   <div class="welcome container">
-    <p>Welcome</p>
+    <div v-if="showLoginForm">
+      <h1>Login</h1>
+      <LoginForm />
+      <p>
+        No account yet?
+        <span @click="showLoginForm = false">Sign up</span> instead
+      </p>
+    </div>
+    <div v-else>
+      <h1>Sign up</h1>
+      <SignupForm />
+      <p>
+        Already registered
+        <span @click="showLoginForm = true">Login</span> instead
+      </p>
+    </div>
   </div>
 </template>
 
 <script>
-export default {}
+import { ref } from 'vue'
+
+import SignupForm from '@/components/SignupForm'
+import LoginForm from '@/components/LoginForm'
+export default {
+  components: {
+    SignupForm,
+    LoginForm,
+  },
+
+  setup() {
+    const showLoginForm = ref(true)
+
+    return {
+      showLoginForm,
+    }
+  },
+}
 </script>
 
 <style>
 .welcome {
-  padding: 20px 0;
   text-align: center;
+  padding: 20px 0;
+}
+/* form styles */
+.welcome form {
+  width: 300px;
+  margin: 20px auto;
+}
+.welcome label {
+  display: block;
+  margin: 20px 0 10px;
+}
+.welcome input {
+  width: 100%;
+  padding: 10px;
+  border-radius: 20px;
+  border: 1px solid #eee;
+  outline: none;
+  color: #999;
+  margin: 10px auto;
 }
 </style>
