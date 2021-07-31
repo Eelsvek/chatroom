d14bca5d5e74052754330ef4f16e41bb8fd0c3ec
Kevs <kev.sunlee@gmail.com>
2021-07-31 14:26:23 -0700

    build: add firebase

---
+++ src/firebase/config.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyD_uYpqxI-vTOkhjqjyuI1oU8dMy2pM9Ww',
+  authDomain: 'chatroom-94057.firebaseapp.com',
+  projectId: 'chatroom-94057',
+  storageBucket: 'chatroom-94057.appspot.com',
+  messagingSenderId: '908777532628',
+  appId: '1:908777532628:web:1adf41f2e34cd18b57df41',
+}
+
+firebase.initializeApp(firebaseConfig)
+
+const projectFirestore = firebase.firestore()
+const timestamp = firebase.firestore.FieldValue.serverTimestamp
+
+export default { projectFirestore, timestamp }
