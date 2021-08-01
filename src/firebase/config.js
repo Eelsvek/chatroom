33314ce14831f2ef84ad4641b8029a85496b11d7
Kevs <kev.sunlee@gmail.com>
2021-08-01 16:16:01 -0700

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,5 +1,6 @@
 import firebase from 'firebase/app'
 import 'firebase/firestore'
+import 'firebase/auth'
 
 const firebaseConfig = {
   apiKey: 'AIzaSyD_uYpqxI-vTOkhjqjyuI1oU8dMy2pM9Ww',
@@ -12,7 +13,8 @@ const firebaseConfig = {
 
 firebase.initializeApp(firebaseConfig)
 
+const projectAuth = firebase.auth()
 const projectFirestore = firebase.firestore()
 const timestamp = firebase.firestore.FieldValue.serverTimestamp
 
-export default { projectFirestore, timestamp }
+export { projectAuth, projectFirestore, timestamp }
